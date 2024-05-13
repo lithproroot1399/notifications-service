d543807af71225054c6a0fda8c808e496262c69a
lithproroot1399 <prorootinfo@gmail.com>
2024-05-13 17:35:33 +0100

    retomando projeto

---
+++ src/application/repositories/notifications-repository.ts
@@ -0,0 +1,6 @@
+import { Notification } from "@application/entities/notification";
+
+export abstract class NotificationsRepository {
+    abstract create(notification: Notification): Promise<void>;
+    abstract findById(notificationId: string): Promise<Notification | null>;
+}
---
+++ test/repositories/in-memory-notifications-repository.ts
@@ -0,0 +1,16 @@
+import { Notification } from "@application/entities/notification";
+import { NotificationsRepository } from "@application/repositories/notifications-repository";
+
+export class InMemoryNotificationsRepository 
+    implements  NotificationsRepository  
+{
+    findById(notificationId: string): Promise<Notification> {
+        throw new Error("Method not implemented.");
+    }  
+    public notifications: Notification[] = [];
+  
+    async create(notification: Notification) {
+    this.notifications.push(notification);
+
+    }
+}
